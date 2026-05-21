991d760d28ae89a9ba2b248b01994b3ced09497d
chandranibha <chandranibha001@gmail.com>
2026-05-21 16:09:17 +0000

    updated restaurant app

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -6,19 +6,10 @@ import CartContext from '../../context/CartContext'
 import './index.css'
 
 const CartItem = ({cartItemDetails}) => {
-  const {
-    dishId,
-    dishName,
-    dishImage,
-    quantity,
-    dishCurrency,
-    dishPrice,
-  } = cartItemDetails
-  const {
-    incrementCartItemQuantity,
-    decrementCartItemQuantity,
-    removeCartItem,
-  } = useContext(CartContext)
+  const {dishId, dishName, dishImage, quantity, dishCurrency, dishPrice} =
+    cartItemDetails
+  const {incrementCartItemQuantity, decrementCartItemQuantity, removeCartItem} =
+    useContext(CartContext)
 
   const onIncreaseQty = () => incrementCartItemQuantity(dishId)
 
@@ -27,26 +18,26 @@ const CartItem = ({cartItemDetails}) => {
   const onRemoveCartItem = () => removeCartItem(dishId)
 
   return (
-    <li className="cart-item-container">
-      <img className="cart-item-image" src={dishImage} alt={dishName} />
-      <div className="cart-item-details">
-        <p className="cart-item-name mb-1">{dishName}</p>
-        <p className="dish-currency-price mt-0 mb-2">
+    <li className='cart-item-container'>
+      <img className='cart-item-image' src={dishImage} alt={dishName} />
+      <div className='cart-item-details'>
+        <p className='cart-item-name mb-1'>{dishName}</p>
+        <p className='dish-currency-price mt-0 mb-2'>
           {dishCurrency} {(quantity * dishPrice).toFixed(2)}
         </p>
-        <div className="control-btn-group">
-          <button type="button" className="control-btn" onClick={onDecreaseQty}>
+        <div className='control-btn-group'>
+          <button type='button' className='control-btn' onClick={onDecreaseQty}>
             -
           </button>
-          <p className="cart-item-quantity">{quantity}</p>
-          <button type="button" className="control-btn" onClick={onIncreaseQty}>
+          <p className='cart-item-quantity'>{quantity}</p>
+          <button type='button' className='control-btn' onClick={onIncreaseQty}>
             +
           </button>
         </div>
       </div>
       <button
-        type="button"
-        className="remove-btn text-danger align-self-center"
+        type='button'
+        className='remove-btn text-danger align-self-center'
         onClick={onRemoveCartItem}
       >
         <FaRegTrashAlt />
